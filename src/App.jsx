455f1c9030b0bf5520f78b3f4ae7bb2c054f97ab
Will Kee <william.w.kee@gmail.com>
2024-01-06 07:48:47 -0700

    hotfix to fix imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Amplify } from "aws-amplify";
 // import { restoreUser } from "./store/session";
-import awsConfig from "./aws-exports";
 import Authentication from "./pages/authentication";
-Amplify.configure(awsConfig);
 
 function App() {
 	const [isLoaded, setIsLoaded] = useState(false);
-	const dispatch = useDispatch();
 
+	// const dispatch = useDispatch();
 	// useEffect(() => {
 	// 	dispatch(restoreUser()).then(() => setIsLoaded(true));
 	// }, [dispatch]);
+
+	useEffect(() => {
+		setIsLoaded(true);
+	}, []);
 
 	const currentUser = useSelector((state) => state.session.user);
 
