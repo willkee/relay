5f590bca87bfc4902eabe9d6c38955067e95dca8
Will Kee <william.w.kee@gmail.com>
2024-02-20 08:15:04 -0700

    production testing

--- src/store/config.js
+++ src/store/config.js
@@ -1,7 +1,7 @@
 let rootUrl;
 
 if (import.meta.env.VITE_VERCEL_ENV === "development") {
-	rootUrl = "http://localhost:5000";
+	rootUrl = "http://localhost:5001/relay-discord/us-central1/default";
 } else if (import.meta.env.VITE_VERCEL_ENV === "production") {
 	rootUrl = "https://relay-discord.web.app";
 } else {
--- src/store/csrf.js
+++ src/store/csrf.js
@@ -1,34 +1,37 @@
+import axios from "axios";
 import Cookies from "js-cookie";
-
+import rootUrl from "./config";
 export async function csrfFetch(url, options = {}) {
 	options.method = options.method || "GET";
 	options.headers = options.headers || {};
 
 	if (options.method.toUpperCase() !== "GET") {
-		if (options.headers["Content-Type"] === "multipart/form-data") {
-			delete options.headers["Content-Type"];
-		} else {
-			options.headers["Content-Type"] =
-				options.headers["Content-Type"] || "application/json";
-		}
-		options.headers["XSRF-TOKEN"] = Cookies.get("XSRF-TOKEN");
+		options.headers["Content-Type"] = "application/json";
+		options.headers["XSRF-Token"] = Cookies.get("XSRF-TOKEN");
+		options.credentials = "include";
 	}
-	const res = await window.fetch(url, options);
+
+	const res = await axios(url, options);
 
 	if (res.status >= 400) {
-		console.error("CSRF Fetch failed!", res);
+		console.error("Axios request failed:", res);
 		throw res;
 	}
-	return await res.json();
+	return await res.data;
 }
 
 export async function restoreCSRF() {
-	const res = await csrfFetch("/api/v1/csrf");
-	Cookies.set("XSRF-TOKEN", res.csrfToken, {
-		expires: 1,
+	const token = Cookies.get("XSRF-TOKEN");
+
+	const res = await axios({
+		method: "GET",
+		url: `${rootUrl}/api/v1/csrf`,
+		credentials: "include",
+		headers: { "XSRF-TOKEN": token },
+	});
+
+	Cookies.set("XSRF-TOKEN", res.data.csrfToken, {
 		path: "/",
 		sameSite: "lax",
-		secure: import.meta.env.VITE_VERCEL_ENV === "production",
-		httpOnly: import.meta.env.VITE_VERCEL_ENV === "production",
 	});
 }
